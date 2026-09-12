d1e0567ab85b4d7e9330956fc0fb9cecb2275692
eliyasramdhan-hub <eliyasramdhan@gmail.com>
2026-09-13 00:53:41 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 import asyncio
 from bot import app, init_db
 
-# تهيئة قاعدة البيانات عند بدء تشغيل الخدمة
-asyncio.get_event_loop().run_until_complete(init_db())
+# تهيئة قاعدة البيانات عند بدء التشغيل
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-# gunicorn سيبحث عن "app"
+loop.run_until_complete(init_db())
+
 application = app
