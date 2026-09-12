89a421e1a96323c3bde73b4b7cb56b6a153c59e4
eliyasramdhan-hub <eliyasramdhan@gmail.com>
2026-09-13 02:18:18 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,3 @@
-import asyncio
-from bot import app, init_db
+from bot import app
 
-# إنشاء حلقة أحداث جديدة وتثبيتها
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-# تهيئة قاعدة البيانات في نفس الحلقة
-loop.run_until_complete(init_db())
-
-# ربط Flask بالحلقة
 application = app
